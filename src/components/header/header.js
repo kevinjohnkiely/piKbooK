1c1f6abd6f8817ffec4a1c51500bd342f7718349
kevinjohnkiely <kevinjohnkiely1@hotmail.com>
2021-03-18 17:04:37 +0000

    first commit

---
+++ src/components/header/header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import classes from './header.module.css'
+import logo from '../../assets/img/logoWhite.png'
+
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <div className="container">
+                <div className={classes.flexHeader}>
+                    <div className={classes.logo}>
+                        <img src={logo} alt=""/>
+                    </div>
+                    <nav className={classes.menu}>
+                        <ul>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">Signup</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/main/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './main.module.css'
+
+const Main = () => {
+    return (
+        <div>
+            <p>Main content</p>
+        </div>
+    )
+}
+
+export default Main
